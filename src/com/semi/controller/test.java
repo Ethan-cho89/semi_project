02381c35295fc9cec6f1ddf76f19bdf5503dcf58
조동현 <@DESKTOP-G9NHE35>
2020-12-16 17:22:27 +0900

    test

---
+++ src/com/semi/controller/test.java
@@ -0,0 +1,14 @@
+package com.semi.controller;
+
+import java.util.ArrayList;
+
+public class test {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		
+		for(int i =0; i<10; i++) {
+			list.add("9");
+		}
+		System.out.println(list.size());
+	}
+}
