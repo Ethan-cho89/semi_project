edbf943b47fecd627b495a823629d42d2565cb37
wm9596 <34181174+wm9596@users.noreply.github.com>
2020-12-20 22:43:03 +0900

    업로드 이미지 관리 스케줄러

--- src/com/semi/task/ImageFileWatchJob.java
+++ src/com/semi/task/ImageFileWatchJob.java
@@ -1,15 +1,60 @@
 package com.semi.task;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-public class ImageFileWatchJob implements Job{
+import com.semi.domain.PhotoVo;
+import com.semi.service.PhotoServiceImpl;
+
+public class ImageFileWatchJob implements Job {
 
 	@Override
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		String root = "/upload/";
-		System.out.println("ImageFileWatchJob");
+	public void execute(JobExecutionContext jc) throws JobExecutionException {
+		deleteFile(jc.getMergedJobDataMap().getString("root"));
 	}
-	
+
+	public void deleteFile(String root) {
+		String rootPath = "/upload/";
+		String tempPath = "/upload/temp";
+
+		List<PhotoVo> list = PhotoServiceImpl.getInstance().getAllList();
+		System.out.println(root);
+		File f = new File(root+rootPath);
+
+		for (File folder : f.listFiles()) {
+			if (!folder.getName().equals("temp") && folder.isDirectory()) {
+				for (File tempFile : folder.listFiles()) {
+					boolean flag = false;
+					for (PhotoVo vo : list) {
+						if (vo.getName().equals(tempFile.getName())) {
+							flag = true;
+						}
+					}
+					if (!flag) {
+						System.out.println(tempFile.getAbsolutePath());
+						tempFile.delete();
+					}
+				}
+				
+				if(folder.listFiles().length<1) {
+					folder.delete();
+				}
+			}
+		}
+
+		f = new File(root+tempPath);
+		
+		for (File ff : f.listFiles()) {
+			System.out.println(ff.getAbsolutePath());
+			ff.delete();
+		}
+		
+	}
 }
--- src/com/semi/task/ImageFileWatchScheduling.java
+++ src/com/semi/task/ImageFileWatchScheduling.java
@@ -3,10 +3,15 @@ package com.semi.task;
 import java.text.ParseException;
 import java.util.Date;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SimpleTrigger;
@@ -18,17 +23,20 @@ import org.quartz.impl.triggers.CronTriggerImpl;
 import org.quartz.impl.triggers.SimpleTriggerImpl;
 
 public class ImageFileWatchScheduling extends HttpServlet{
-
+	Scheduler scheduler ;
 	@Override
 	public void init() throws ServletException {
+		
 		try {
-			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+			scheduler = new StdSchedulerFactory().getScheduler();
 			scheduler.start();
 			
-			JobDetail detail = new JobDetailImpl("ImageFileWatch",Scheduler.DEFAULT_GROUP,ImageFileWatchJob.class);
+			JobDataMap map = new JobDataMap();
+			map.put("root",getServletContext().getRealPath(""));
 			
+			JobDetail detail = JobBuilder.newJob(ImageFileWatchJob.class).setJobData(map).withIdentity("job", Scheduler.DEFAULT_GROUP).build();
 //			Trigger trigger = new CronTriggerImpl("trigger",Scheduler.DEFAULT_GROUP,"0 0 12 * * ?");
-			Trigger trigger = new CronTriggerImpl("trigger",Scheduler.DEFAULT_GROUP,"1 20 * * * ?");
+			Trigger trigger = new CronTriggerImpl("trigger",Scheduler.DEFAULT_GROUP,"0 0 0 * * ?");
 			
 			scheduler.scheduleJob(detail,trigger);
 			
@@ -41,4 +49,16 @@ public class ImageFileWatchScheduling extends HttpServlet{
 			e.printStackTrace();
 		}
 	}
+	
+	@Override
+	public void destroy() {
+		try {
+			scheduler.shutdown(true);
+			System.out.println("종료");
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
